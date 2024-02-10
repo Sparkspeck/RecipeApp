ca306d603dc45b453f7355882247cac76dcff719
Sparkspeck <gouthamgs2709@gmail.com>
2024-02-10 16:51:41 +0530

    Initial Commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable() //In order to use a shoping list service in recipe service we use Injectable, this is done so we can send our ingredients to the shopping list
+
+export class RecipeService{
+
+    recipeSelected  = new EventEmitter<Recipe>();
+
+    private recipes : Recipe[] = [          //This object has been defined in recipe.model.ts
+    new Recipe('Burger','A juicy hamburger','https://www.shutterstock.com/image-photo/classic-hamburger-stock-photo-isolated-600nw-2282033179.jpg',[
+      
+      new Ingredient("Buns",2),
+      new Ingredient("Meat",1)
+    ]),
+    new Recipe('Schnitzel','A tasty schnitzel','https://staticcookist.akamaized.net/wp-content/uploads/sites/22/2021/05/thumb-1200x675.jpg',[
+      new Ingredient("Meat",1),
+      new Ingredient("French Fries",20)
+    ])
+
+  ]; 
+
+  constructor(private slService:ShoppingListService){}
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingLIst(ingredients : Ingredient[]){
+    this.slService.addIngredients(ingredients);
+  }
+}
